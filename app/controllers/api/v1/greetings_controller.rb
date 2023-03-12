515b975edf1923e688af62e62f8a152df2598452
Daphineatim <hopedaphine19@gmail.com>
2023-03-12 03:02:04 -0700

    Fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
     if @greetings.empty?
-        json_response({ msg: 'no greeting found' }, 400)
+      json_response({ msg: 'no greeting found' }, 400)
     else
-        render json: @greetings[rand(0...@greetings.length - 1)] 
-    end  
+      render json: @greetings[rand(0...@greetings.length - 1)]
+    end
   end
 end
