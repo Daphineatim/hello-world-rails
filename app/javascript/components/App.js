9eee2c50e2b56675e3dac6a7c42c3ffe4516a307
Daphineatim <hopedaphine19@gmail.com>
2023-03-12 02:55:20 -0700

    Update App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -13,18 +13,8 @@ const App = () => {
     <BrowserRouter>
     <Header />
     <Routes>
-        <Route
-            path="/"
-            element={(
-                <Home />
-            )}
-            />
-        <Route
-            path="/greeting"
-            element={(
-                <Greeting />
-            )}
-            />
+    <Route path="/" element={( <Home /> )} />
+    <Route path="/greeting" element={( <Greeting /> )} />
     </Routes>
 </BrowserRouter>
 </Provider>
