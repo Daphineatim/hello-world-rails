4c8a3b623ff0c45d5e60f20e5b89c801f20a6885
Daphineatim <hopedaphine19@gmail.com>
2023-03-25 07:54:57 -0700

    Make message display

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App';
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
